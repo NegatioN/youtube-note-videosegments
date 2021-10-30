799d236a1a9cbd8089719beeab3f1f4e73b9e63d
NegatioN <joakimrishaug@gmail.com>
2021-10-30 15:23:58 +0200

    pivot

--- run.py
+++ run.py
@@ -1,41 +1,28 @@
-import sqlite3
-import snappy
+import subprocess
+from youtube_dl import YoutubeDL
 import json
 from urllib.parse import urlparse, parse_qs
 
-ext_uuid = '8bf5521e-d809-46aa-8565-5fb1092aed03'
-con = sqlite3.connect(f'/home/joakim/.mozilla/firefox/ycbl2bwi.default-release/storage/default/moz-extension+++{ext_uuid}^userContextId=4294967295/idb/3647222921wleabcEoxlt-eengsairo.sqlite')
-cur = con.cursor()
+def get_download_link(link):
+    with YoutubeDL() as y:
+        r = y.extract_info(link, download=False)
+        return r['formats'][-1]['url']
 
-for i, row in enumerate(cur.execute('SELECT * FROM object_data;')):
-    name = bytes([b-1 for b in row[1][1:]])
-    print(name, snappy.decompress(row[4]))
-    print(row[1], row[4])
 
 def to_seconds(time_string):
     mins, secs = time_string.strip().split(':')
-
     return (int(mins) * 60) + int(secs)
 
 def construct_timestamps(s):
-    prefix = b'\x03\x00\x00\x00\x00\x00\xf1\xffL\x00\x00\x80\x04\x00\xff\xff'
-    postfix = b'\x00\x00\x00\x00'
     stride = 3
     entries = s.split('::')
-    # timestamps in selections need to be inverted to skip the other parts of the video
-    timestamps = [0.]
-    types = ['c']
+    timestamps = []
+    print(entries)
     for i in range(0, len(entries), stride):
         start, end = entries[i].split('-')
-        timestamps.append(to_seconds(start))
-        timestamps.append(to_seconds(end))
-        types.append('cs')
-        types.append('c')
-    types.append()
-    types.append('-')
-    data = {'timestamps': timestamps, 'types': types}
-    return snappy.compress(prefix +json.dumps(data, separators=(',', ':')).encode('utf-8') + postfix)
+        timestamps.append((to_seconds(start), to_seconds(end)))
 
+    return timestamps
 
 def parse_document(path):
     '''
@@ -46,27 +33,38 @@ def parse_document(path):
     '''
     with open(path, 'r') as f:
         url = f.readline().strip()
-        url_id = parse_qs(urlparse(url).query)['v'][0]
-        video_id = bytes([48] + [b+1 for b in bytes(f'youtube-{url_id}', 'utf-8')])
         content = f.read()
-
         timestamps = construct_timestamps(content)
-    return video_id, timestamps
+    return url, timestamps
 
-#TODO figure out what \xffL part difference is because of. Some sort of padding probably.
-#\x03\x00\x00\x00\x00\x00\xf1\xff9\x00\x00\x80\x04\x00\xff\xff
-#\x00\x00\x00\x00\x00\x00\x00
+video_url, timestamps = parse_document('/home/joakim/Desktop/maps_of_meaning.txt')
 
-#\x03\x00\x00\x00\x00\x00\xf1\xffa\x00\x00\x80\x04\x00\xff\xff
-#\x00\x00\x00\x00\x00\x00\x00
-video_id, timestamps = parse_document('/home/joakim/Desktop/maps_of_meaning.txt')
+print(video_url)
+print(timestamps)
 
+true_url = get_download_link(video_url)
 
-#cur.execute("drop trigger IF EXISTS object_data_insert_trigger")
-#cur.execute("insert into object_data(object_store_id, key, data) values (?, ?, ?)", (1, video_id, timestamps))
-con.commit()
-cur.close()
+fade = 0.05
+# Build ffmpeg command
 
+#target_file = 'video.mp4'
+target_file = true_url
 
+COMMAND = 'ffmpeg '
+for start, end in timestamps:
+    COMMAND += f'-ss {start} -to {end} -i "{target_file}" ' # Not fast-seeking seems incredibly slow, so thats why we do this
 
-#drop trigger object_data_insert_trigger; must be in place to insert stuff.
+complex_filter = '[0:v]setpts=PTS-STARTPTS[0v];[0:a]asetpts=PTS-STARTPTS[0a];'
+voffset = (timestamps[0][1] - timestamps[0][0]) - fade
+
+for i, (start, end) in enumerate(timestamps[1:], 1):
+    complex_filter += f'[{i}:v]setpts=PTS-STARTPTS[{i}v];[{i}:a]asetpts=PTS-STARTPTS[{i}a];'
+    complex_filter += f'[{i-1}v][{i}v]xfade=offset={voffset:.3f}:duration={fade:.3f}[{i}v];[{i-1}a][{i}a]acrossfade=duration={fade}[{i}a];'
+    voffset += (end - start) - fade
+
+    COMMAND = f"{COMMAND} -filter_complex '{complex_filter[:-1]}' -map '[{i}v]' -map '[{i}a]' output.mp4"
+
+#PLAN: https://unix.stackexchange.com/questions/230481/how-to-download-portion-of-video-with-youtube-dl-command
+
+#output = subprocess.run(COMMAND, shell=True, capture_output=True)
+print(COMMAND)
